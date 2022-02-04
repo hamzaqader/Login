ca80332373cc4715837450f9fccb62e64af7dd27
hamzaqader <57059513+hamzaqader@users.noreply.github.com>
2022-02-04 14:05:53 +0500

    changess

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,9 +7,6 @@ function Login(){
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const dispatch = useDispatch();
-  
-    const userLogin = useSelector((state) => state.userLogin);
-
     const history= useHistory();
   const submitHandler = (e) => {
     e.preventDefault();
