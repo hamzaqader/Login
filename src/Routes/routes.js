1bffb8fba4748ce566f12d5b90fc4989129e448b
hamzaqader <57059513+hamzaqader@users.noreply.github.com>
2022-02-04 16:54:19 +0500

    ok

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,18 +1,18 @@
-import { BrowserRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Login from '../components/Login';
 import Register from "../components/register";
 import { PrivateRoute } from '../components';
-import {PublicRoute} from '../components'
+import { PublicRoute } from '../components'
 import Dashboard from '../components/Dashboard'
 function Routes() {
     return (
         <Router >
-                            <div>
-                                <PublicRoute  exact path="/" component={Login} />
-                                <PrivateRoute path="/dashboard" component={Dashboard} />
-                                <PublicRoute path="/register" component={Register} />
-                            </div>
-                        </Router>
+            <div>
+                <PublicRoute exact path="/" component={Login} />
+                <PrivateRoute path="/dashboard" component={Dashboard} />
+                <PublicRoute path="/register" component={Register} />
+            </div>
+        </Router>
     );
 }
 
